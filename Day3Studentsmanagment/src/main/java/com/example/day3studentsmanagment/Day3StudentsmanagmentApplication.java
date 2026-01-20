fd195e2f46d4d02212225edd2b5d7e6f4dfd5e9a
Harshit-295 <bhardwajharsh1984@gmail.com>
2026-01-20 11:56:27 +0530

    first commit

---
+++ Day3Studentsmanagment/src/main/java/com/example/day3studentsmanagment/Day3StudentsmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3studentsmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3StudentsmanagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3StudentsmanagmentApplication.class, args);
+    }
+
+}
---
+++ Day3Studentsmanagment/src/test/java/com/example/day3studentsmanagment/Day3StudentsmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3studentsmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3StudentsmanagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
